9af1287292b3057c7083cf6ca6d930d7815efdb2
Lorenzo Saino <lsaino@fastly.com>
2018-11-13 18:13:47 -0800

    Add functions to parse sysctl.conf files
    Add functions to parse sysctl configuration files and set the sysctl
    values read from configuration files.

---
+++ conf.go
@@ -0,0 +1,74 @@
+package sysctl
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const sysctlConfPath = "/etc/sysctl.conf"
+
+func readFromConfigFile(path string, out map[string]string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("could not open file: %s", err.Error())
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parsed := strings.Split(line, "#")[0]
+		parsed = strings.Split(parsed, ";")[0]
+		parsed = strings.TrimSpace(parsed)
+		if parsed == "" {
+			continue
+		}
+		tokens := strings.Split(parsed, " = ")
+		if len(tokens) != 2 {
+			return fmt.Errorf("could not parse line %s", line)
+		}
+		k := strings.TrimSpace(tokens[0])
+		v := strings.TrimSpace(tokens[1])
+		out[k] = v
+	}
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("error reading file: %s", err.Error())
+	}
+
+	return nil
+}
+
+// GetFromFiles gets sysctl values from a list of sysctl configuration files.
+// The values in the rightmost files take priority.
+// If no file is specified, values are read from /etc/sysctl.conf.
+func GetFromFiles(files ...string) (map[string]string, error) {
+	if len(files) == 0 {
+		files = []string{sysctlConfPath}
+	}
+	out := make(map[string]string)
+	for _, f := range files {
+		if err := readFromConfigFile(f, out); err != nil {
+			return nil, fmt.Errorf("could not parse file %s: %s", f, err.Error())
+		}
+	}
+	return out, nil
+}
+
+// SetFromFiles sets sysctl values from a list of sysctl configuration files.
+// The values in the rightmost files take priority.
+// If no file is specified, values are read from /etc/sysctl.conf.
+func SetFromFiles(files ...string) error {
+	config, err := GetFromFiles(files...)
+	if err != nil {
+		return fmt.Errorf("could not read configuration from files: %s", err.Error())
+	}
+	for k, v := range config {
+		if err := Set(k, v); err != nil {
+			return fmt.Errorf("could not set %s = %s: %s", k, v, err.Error())
+		}
+	}
+	return nil
+}
---
+++ conf_test.go
@@ -0,0 +1,76 @@
+package sysctl
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReadConfigFile(t *testing.T) {
+	cases := []struct {
+		path string
+		err  bool
+		out  map[string]string
+	}{
+		{
+			path: "testdata/sysctl-correct.conf",
+			err:  false,
+			out: map[string]string{
+				"kernel.domainname": "example.com",
+				"kernel.modprobe":   "/sbin/mod probe",
+				"kernel.hostname":   "example.com",
+			},
+		},
+		{
+			path: "testdata/sysctl-empty.conf",
+			err:  false,
+			out:  map[string]string{},
+		},
+		{
+			path: "testdata/sysctl-error.conf",
+			err:  true,
+		},
+	}
+	for _, c := range cases {
+		out := make(map[string]string)
+		if err := readFromConfigFile(c.path, out); err != nil {
+			if !c.err {
+				t.Errorf("expected error when parsing %s but it succeeded", c.path)
+			}
+			continue
+		}
+		if !reflect.DeepEqual(out, c.out) {
+			t.Errorf("unexpected output from %s. Expected: %s, got: %s", c.path, c.out, out)
+		}
+	}
+}
+
+func TestGetFromFiles(t *testing.T) {
+	cases := []struct {
+		paths []string
+		err   bool
+		out   map[string]string
+	}{
+		{
+			paths: []string{
+				"testdata/sysctl-a.conf",
+				"testdata/sysctl-b.conf",
+			},
+			err: false,
+			out: map[string]string{
+				"kernel.domainname": "b.com",
+			},
+		},
+	}
+	for _, c := range cases {
+		out, err := GetFromFiles(c.paths...)
+		if err != nil {
+			if !c.err {
+				t.Errorf("expected error when parsing %s but it succeeded", c.paths)
+			}
+			continue
+		}
+		if !reflect.DeepEqual(out, c.out) {
+			t.Errorf("unexpected output from %s. Expected: %s, got: %s", c.paths, c.out, out)
+		}
+	}
+}
